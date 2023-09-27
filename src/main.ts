3aa5232b263e0eb5e721a8f3f3b2b703c4dbeea2
gidskql6671 <gidskql6671@gmail.com>
2023-09-27 15:49:15 +0900

    Swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 8080;
+
+  const config = new DocumentBuilder()
+    .setTitle('KNU AI')
+    .setDescription('KNU AI 서비스의 API Documents입니다.')
+    .setVersion('1.0')
+    .addTag('KNU AI')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
 
