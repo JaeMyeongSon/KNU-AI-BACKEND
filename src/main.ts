034282bd2a9a3746c048066f064ad5a72d02291d
gidskql6671 <gidskql6671@gmail.com>
2023-09-27 15:47:09 +0900

    Port 및 Application 설정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as process from 'process';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const port = process.env.PORT || 8080;
+
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
