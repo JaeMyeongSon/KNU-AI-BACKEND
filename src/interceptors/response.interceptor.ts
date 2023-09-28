f11f12b3163ac5a413b1858236f7cbaa75c2dc79
gidskql6671 <gidskql6671@gmail.com>
2023-09-28 11:27:54 +0900

    Response Type 통일을 위한 글로벌 인터셉터 선언
    데이터를 { data: data }의 형태로 반환한다.
    추후 해당 json 객체에 code 등의 추가정보를 넣을 수 있다.

---
+++ src/interceptors/response.interceptor.ts
@@ -0,0 +1,13 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next
+      .handle()
+      .pipe(map((data) => ({ data: data === undefined ? null : data })));
+  }
+}
