c4d6f43d71d35cd2d69fa64df2fc34d511b834ff
gidskql6671 <gidskql6671@gmail.com>
2023-09-28 11:59:50 +0900

    Create message api의 응답 api docs 추가

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { OpenaiClientService } from '../openai-client/openai-client.service';
 import { CreateMessageRequestDto } from './dto/create-message-request.dto';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateMessageResponseSwagger } from './dto/create-message-response.swagger';
 
 @ApiTags('Chats')
 @Controller('api/chats')
@@ -10,7 +11,10 @@ export class ChatsController {
 
   @Post('messages')
   @ApiOperation({ summary: '챗봇에게 채팅 메시지 전송' })
-  @ApiOkResponse({ description: '챗봇의 응답 메시지' })
+  @ApiOkResponse({
+    description: '챗봇의 응답 메시지',
+    type: CreateMessageResponseSwagger,
+  })
   async createMessage(@Body() content: CreateMessageRequestDto) {
     const { message } = content;
 
