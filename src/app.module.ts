ed2028f2f6456cd5ff9e5a7a2f3eccc493392f83
gidskql6671 <gidskql6671@gmail.com>
2023-09-28 11:08:23 +0900

    naming Rules 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { OpenaiClientModule } from './openai_client/openai_client.module';
+import { OpenaiClientModule } from './openai-client/openai-client.module';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), OpenaiClientModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    OpenaiClientModule,
+    ChatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
