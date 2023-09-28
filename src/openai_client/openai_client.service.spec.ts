ce618030a7cb0a9b845e721027bd6018bb9e5b2c
gidskql6671 <gidskql6671@gmail.com>
2023-09-28 10:56:40 +0900

    openai client 모듈 구축

---
+++ src/openai_client/openai_client.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { OpenaiClientService } from './openai_client.service';
+
+describe('OpenaiClientService', () => {
+  let service: OpenaiClientService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [OpenaiClientService],
+    }).compile();
+
+    service = module.get<OpenaiClientService>(OpenaiClientService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
