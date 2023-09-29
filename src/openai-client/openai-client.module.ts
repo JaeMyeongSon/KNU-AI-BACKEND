fd58936c94f564ac60634a03f026014c0b1ee4ca
gidskql6671 <gidskql6671@gmail.com>
2023-09-29 16:09:47 +0900

    디비에서 Chatbot Role 관리

--- src/openai-client/openai-client.module.ts
+++ src/openai-client/openai-client.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OpenaiClientService } from './openai-client.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Chatbot, ChatbotSchema } from '../schemas/chatbot.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Chatbot.name, schema: ChatbotSchema }]),
+  ],
   providers: [OpenaiClientService],
   exports: [OpenaiClientService],
 })
---
+++ src/schemas/chatbot.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ChatbotRole } from '../openai-client/chatbot.role';
+import { OpenaiMessageDto } from '../openai-client/dto/openai-message.dto';
+
+@Schema()
+export class Chatbot {
+  @Prop({
+    required: true,
+    unique: true,
+    enum: Object.values(ChatbotRole),
+  })
+  role: string;
+
+  @Prop({
+    required: true,
+  })
+  setupMessages: OpenaiMessageDto[];
+}
+
+export const ChatbotSchema = SchemaFactory.createForClass(Chatbot);
