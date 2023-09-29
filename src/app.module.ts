f549a3689abee84235ea2697df545b23c3b44357
gidskql6671 <gidskql6671@gmail.com>
2023-09-29 15:50:07 +0900

    mongoose 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { OpenaiClientModule } from './openai-client/openai-client.module';
 import { ChatsModule } from './chats/chats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'process';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
     OpenaiClientModule,
     ChatsModule,
   ],
