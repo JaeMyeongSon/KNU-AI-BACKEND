ed77da769872ab162616369b65b70b295b291d37
gidskql6671 <gidskql6671@gmail.com>
2023-10-01 23:30:03 +0900

    챗봇 목록 조회 API 추가 및 챗봇 역할 목록 조회 API 삭제

---
+++ src/chatbots/dto/chatbot.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ChatbotRole } from '../../openai-client/chatbot.role';
+import { Chatbot } from '../../schemas/chatbot.schema';
+
+export class ChatbotDto {
+  static fromSchema(chatbot: Chatbot & { id?: any }) {
+    const chatbotDto = new ChatbotDto();
+
+    chatbotDto.id = chatbot.id;
+    chatbotDto.name = chatbot.name;
+    chatbotDto.role = chatbot.role;
+
+    return chatbotDto;
+  }
+
+  @ApiProperty({
+    required: true,
+    description: '챗봇의 ID',
+  })
+  id: string;
+
+  @ApiProperty({
+    required: true,
+    description: '챗봇의 이름',
+  })
+  name: string;
+
+  @ApiProperty({
+    required: true,
+    type: String,
+    enum: ChatbotRole,
+    description: '챗봇의 역할',
+  })
+  role: string;
+}
