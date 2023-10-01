5d205095e0d135a338ad3e48f87abf4ab9203278
oyoungsun <oyoungsun1030@gmail.com>
2023-10-01 14:09:08 +0900

    #12 - user service 기본  작성

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async getUser(userName: string): Promise<any> {
+    try {
+      const result = await this.userModel.findOne({ userName }).lean();
+      return result;
+    } catch (err) {
+      console.log('error...');
+    }
+  }
+}
