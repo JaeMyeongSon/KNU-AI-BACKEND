fbcd21b618859d02875fb755bf790d4f393bac01
gidskql6671 <gidskql6671@gmail.com>
2023-10-03 18:13:38 +0900

    MYSQL & TypeOrm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,25 @@ import { OpenaiClientModule } from './openai-client/openai-client.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatbotsModule } from './chatbots/chatbots.module';
 import * as process from 'process';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_DB_URL),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_DB_HOST,
+      port: +process.env.MYSQL_DB_PORT,
+      username: process.env.MYSQL_DB_USERNAME,
+      password: process.env.MYSQL_DB_PASSWORD,
+      database: process.env.MYSQL_DB_DATABASE,
+      entities: [],
+      logging: true,
+      keepConnectionAlive: true,
+      synchronize: true,
+      charset: 'utf8mb4',
+    }),
     OpenaiClientModule,
     ChatbotsModule,
   ],
