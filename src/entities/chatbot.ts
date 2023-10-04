29f339178d29954906a4e76fd119194f840527d6
gidskql6671 <gidskql6671@gmail.com>
2023-10-04 14:37:32 +0900

    챗봇 Role Enum을 제거함.
    사용했을 때 장점이라면, 코드 상에서 어떤 role이 있는지 명확하게 볼 수 있는 것이지만...
    데이터와 코드가 강결합되는 것이 좋지 않다고 생각되어 제거함

--- src/entities/chatbot.ts
+++ src/entities/chatbot.ts
@@ -1,5 +1,4 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { ChatbotRole } from '../chatbots/chatbot.role';
 import { ChatbotSetupMessage } from './chatbot-setup-message';
 
 @Entity({ name: 'chatbots' })
@@ -7,8 +6,8 @@ export class Chatbot {
   @PrimaryGeneratedColumn({ type: 'bigint', name: 'id' })
   id: number;
 
-  @Column({ type: 'enum', enum: ChatbotRole })
-  role: ChatbotRole;
+  @Column()
+  role: string;
 
   @Column('varchar')
   name: string;
