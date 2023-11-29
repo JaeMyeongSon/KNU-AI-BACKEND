76cb6aac031964dfa151e57bcb964e989db75fb2
gidskql6671 <gidskql6671@gmail.com>
2023-11-29 14:36:18 +0900

    예외 처리 핸들러 다듬기

--- src/common/exception-filters/ws.exception-filter.ts
+++ src/common/exception-filters/ws.exception-filter.ts
@@ -1,25 +1,15 @@
-import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
+import { ArgumentsHost, Catch } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { WsException } from '@nestjs/websockets';
 
-@Catch(WsException, HttpException)
+@Catch(WsException)
 export class WsExceptionFilter {
-  public catch(exception: HttpException, host: ArgumentsHost) {
+  public catch(exception: WsException, host: ArgumentsHost) {
     const client = host.switchToWs().getClient();
     this.handleError(client, exception);
   }
 
-  public handleError(client: Socket, exception: HttpException | WsException) {
-    if (exception instanceof HttpException) {
-      const res = exception.getResponse();
-
-      if (res.hasOwnProperty('message')) {
-        client.emit('error', (res as { message: string }).message);
-      } else {
-        client.emit('error', exception.message);
-      }
-    } else {
-      client.emit('error', exception.message);
-    }
+  public handleError(client: Socket, exception: WsException) {
+    client.emit('error', exception.message);
   }
 }
