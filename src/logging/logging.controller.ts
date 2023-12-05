2d33c24a18ddbc8f877a378f0ee536824199744a
oyoungsun <oyoungsun1030@gmail.com>
2023-12-05 17:46:31 +0900

    feat(LoggingController) : 로그가 DB에 저장되는지 확인하기 위한 API 추가

--- src/logging/logging.controller.ts
+++ src/logging/logging.controller.ts
@@ -1,15 +1,19 @@
-import { AppService } from 'src/logging/app.service';
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { LoggingService } from './logging.service';
 
 @ApiTags('logging')
 @Controller('logging')
 export class LoggingController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly mylogger: LoggingService) {}
 
   @Get()
   async loggingTest() {
-    console.log('로깅 출력 검사 : ');
-    this.appService.getHello();
+    this.mylogger.error('this is error테스트입니다');
+    this.mylogger.log('this is log테스트입니다');
+    this.mylogger.debug('debug 로그 테스트');
+    this.mylogger.warn('warn 테스트');
+    this.mylogger.verbose('verbos 테스트');
+    // this.appService.getHello();
   }
 }
