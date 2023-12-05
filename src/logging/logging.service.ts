e8c5afff79612d2889fdaf1e5bdc9984a40d0534
oyoungsun <oyoungsun1030@gmail.com>
2023-12-05 17:46:09 +0900

    feat(LoggingService) : MongoDb에 로그 기록 저장하는 로직 구현

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,24 +1,54 @@
 import { Injectable, LoggerService } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Logging } from 'src/entities/logging';
 
 @Injectable()
 export class LoggingService implements LoggerService {
-  log(message: any, ...optionalParams: any[]) {
-    console.log(message);
+  constructor(
+    @InjectModel(Logging.name) private loggingModel: Model<Logging>,
+  ) {}
+
+  async log(message: any) {
+    const createLog = await this.loggingModel.create({
+      level: Level.log,
+      message: message,
+    });
   }
 
-  error(message: any, ...optionalParams: any[]) {
-    console.log(message);
+  async error(message: any, ...optionalParams: any[]) {
+    const createError = await this.loggingModel.create({
+      level: Level.error,
+      message: message,
+    });
   }
 
-  warn(message: any, ...optionalParams: any[]) {
-    console.log(message);
+  async warn(message: any, ...optionalParams: any[]) {
+    const createWarn = await this.loggingModel.create({
+      level: Level.warn,
+      message: message,
+    });
   }
 
-  debug(message: any, ...optionalParams: any[]) {
-    console.log(message);
+  async debug(message: any, ...optionalParams: any[]) {
+    const createDebug = await this.loggingModel.create({
+      level: Level.debug,
+      message: message,
+    });
   }
 
-  verbose(message: any, ...optionalParams: any[]) {
-    console.log(message);
+  async verbose(message: any, ...optionalParams: any[]) {
+    const createVerbose = await this.loggingModel.create({
+      level: Level.verbose,
+      message: message,
+    });
   }
+}
+
+enum Level {
+  log = '[LOG]',
+  error = '[ERROR]',
+  warn = '[WARN]',
+  debug = '[DEBUG]',
+  verbose = '[VERBOSE]',
 }
